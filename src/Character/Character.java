e579129517db6ec586e001352dcd9cf8a3890733
Andy Tung <andyytung@gmail.com>
2015-01-12 18:15:03 -0800

    dont know what to do now

--- src/Character/Character.java
+++ src/Character/Character.java
@@ -1,10 +1,15 @@
 package Character;
 
 public class Character {
-	String name;
-	Roles role;
-	
+	private String name;
+	private Roles role;
+	private boolean alive;
+
 	Character(String name, Roles role){
-		
+		this.name = name;
+		this.role = role;
+		alive = true;
 	}
+	
+	
 }
