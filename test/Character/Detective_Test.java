24c8e0423d9409f2f7fc2bee02d34061a628c703
Derek Chan <derek.chan@alumni.ubc.ca>
2015-01-14 23:32:12 -0800

    Modified the Detective_Test to make sure that everything works

--- test/Character/Detective_Test.java
+++ test/Character/Detective_Test.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import junit.framework.Assert;
 
-import org.junit.Before;
 import org.junit.Test;
-import GameEngine.*;
+import GameEngine.GameEngine;
 import GameEngine.Character;
+import GameEngine.Player;
 import Character.Detective;
 import Character.Roles;
 
@@ -16,20 +16,28 @@ public class Detective_Test {
 	
 	@Test
 	public void testAction() {
+		// do admin things: register players and assign roles
 		GameEngine.registerPlayer("Derek");
 		GameEngine.registerPlayer("Harris");
 		GameEngine.assignCharacter(Player.get("Derek"), new Detective());
 		GameEngine.assignCharacter(Player.get("Harris"), new Dork());
 		
+		// make Derek target Harris
 		List<Player> target = new ArrayList<Player>();
 		target.add(Player.get("Harris"));
 		Assert.assertTrue( GameEngine.setTarget(Player.get("Derek"), target) );
+		
+		// make Derek investigate Harris
 		Assert.assertEquals( GameEngine.getCharacter(Player.get("Derek")).doAction(),
 				"Player Harris is a Dork.");
 	}
 
 	/**
 	 * Gotta keep it professional.
+	 * 
+	 * Private shell class Dork to make sure that the Detective returns the
+	 * correct Class string. It's methods do nothing since they're never called.
+	 * They don't need to be implemented.
 	 */
 	private class Dork extends Character {
 
@@ -39,15 +47,12 @@ public class Detective_Test {
 
 		@Override
 		public boolean setTarget(List<Player> targets) {
-			// TODO Auto-generated method stub
 			return false;
 		}
 
 		@Override
 		public String doAction() {
-			// TODO Auto-generated method stub
 			return null;
-		}
-		
+		}		
 	}
 }
