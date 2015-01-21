e14d5c3445188c4bbaea398c667a9eb18ce0da6a
Derek Chan <derek.chan@alumni.ubc.ca>
2015-01-21 01:07:54 -0800

    Cleaned up println statements in DatabaseManager

--- test/GameEngine/DatabaseManager_Test.java
+++ test/GameEngine/DatabaseManager_Test.java
@@ -1,5 +1,7 @@
 package GameEngine;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import Character.Investigator;
@@ -11,24 +13,25 @@ public class DatabaseManager_Test {
 	@Test
 	public void test_initReadAndWrite() {
 		GameEngine.registerPlayer("Derek");
-		
+
 		try {
 			GameEngine.assignCharacter(Player.get("Derek"), new Investigator(Player.get("Derek")));
 		} catch (CannotGetPlayerException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		System.out.println("Initializing...");
 		DatabaseManager.init();
+
 		try {
-			System.out.println("Getting player...");
 			Player p = Player.get("Derek");
-			System.out.println("Getting character...");
 			Character c = GameEngine.getCharacter(Player.get("Derek"));
 			System.out.println("Adding data...");
 			DatabaseManager.addData(p, c);
 			System.out.println("Retrieving data...");
 			DatabaseManager.getData(Player.get("Derek"));
-		} catch (Exception e) {	}
+		} catch (Exception e) { 
+			fail("Something went wrong.");
+		}
 	}
 }
