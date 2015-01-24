fcfe2cbc3988b5af9076fa7d10500412dd15570e
Eleanor Wong <eleanorwong@live.ca>
2015-01-24 01:27:26 -0800

    Barebones Lookout implementation

--- src/Character/Lookout.java
+++ src/Character/Lookout.java
@@ -5,6 +5,7 @@ import java.util.List;
 import Enumerators.Roles;
 import GameEngine.Character;
 import GameEngine.GameEngine;
+import GameEngine.GameMessage;
 import GameEngine.Player;
 
 public class Lookout extends Character {
@@ -14,6 +15,7 @@ public class Lookout extends Character {
 
 	@Override
 	public boolean setTarget(List<Player> targets) {
+		// The detective can only target a single Player
 		if (targets.size() != 1
 				|| !GameEngine.alive_player.containsAll(targets)) {
 			return false;
@@ -23,7 +25,31 @@ public class Lookout extends Character {
 
 	@Override
 	public String doAction() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Player> targets = getTargets();
+		if (targets.isEmpty()) {
+			return GameMessage.NO_ACTION;
+		}
+		String message = "Cannot get your results";
+		if (this.isRoleBlocked()) {
+			return message;
+		}
+		GameEngine.getCharacter(targets.get(0)).addVisitor(getPlayer());
+		
+		// Retrieve the list of who Detective Target visited
+		List<Player> tracker = GameEngine.getCharacter(targets.get(0)).getVistors();
+		
+		
+		//TODO: Include option for ignoring detection immunity
+		//      Add exception that lookout does not die to veteran ***
+		//      ALSO any bus driver exceptions
+		//TODO: Make it so that it doesn't say you visited your target because realy
+		String result = "Your target was visited by ";
+		for ( int i = 0; i < tracker.size(); i++ ) {
+			
+				result = result + tracker.get(i).toString() + ",";
+			
+		}
+		
+		return result;
 	}
 }
